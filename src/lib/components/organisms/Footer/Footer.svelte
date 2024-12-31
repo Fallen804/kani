f02cf37de5b102ab74b9b3050be22b1bcd315165
Maximilian <18749130+Bluuax@users.noreply.github.com>
2024-12-31 21:33:56 +0100

    refactor: footer

--- src/lib/components/organisms/Footer/Footer.svelte
+++ src/lib/components/organisms/Footer/Footer.svelte
@@ -1,14 +1,18 @@
+<script lang="ts"></script>
+
 <hr class="mx-auto mb-4 mt-10 w-5/6 border-zinc-300" />
 
 <footer class="mx-auto mb-4 w-5/6">
 	<nav>
 		<ul class="flex justify-center gap-4">
-			<li>
-				<a href="/privacy" class="hover:text-indigo-500">Privacy</a>
-			</li>
-			<li>
-				<a href="/imprint" class="hover:text-indigo-500">Imprint</a>
-			</li>
+			{#snippet link(url: string, name: string)}
+				<li>
+					<a href="/{url}" class="hover:text-indigo-500">{name}</a>
+				</li>
+			{/snippet}
+
+			{@render link('privacy', 'Privacy')}
+			{@render link('imprint', 'Imprint')}
 		</ul>
 	</nav>
 </footer>
