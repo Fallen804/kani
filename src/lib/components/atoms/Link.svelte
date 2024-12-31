89841f38afa63dfa2092df9c82e5b8ca19e1d14b
Maximilian <18749130+Bluuax@users.noreply.github.com>
2024-12-31 22:13:27 +0100

    feat: add Link component

---
+++ src/lib/components/atoms/Link.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { children, url } = $props();
+	let externalUrl = $derived(url.startsWith('http'));
+</script>
+
+{#if children}
+	<a
+		href={url}
+		target={externalUrl ? '_blank' : ''}
+		rel={externalUrl ? 'noopener noreferrer' : ''}
+		class="text-indigo-500 hover:text-indigo-800"
+	>
+		{@render children()}
+	</a>
+{/if}
--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import Link from '$lib/components/atoms/Link.svelte';
+</script>
+
 <svelte:head>
 	<title>Privacy</title>
 </svelte:head>
@@ -13,23 +17,16 @@
 	<p class="mb-5 text-lg sm:text-xl">
 		This website is hosted on Vercel, which generates logs that may include the date and time of
 		your request, the requested resource, and your anonymized IP address. For more information,
-		please refer to <a
-			href="https://vercel.com/legal/privacy-policy"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="text-indigo-500">Vercel's privacy policy</a
-		>.
+		please refer to
+		<Link url="https://vercel.com/legal/privacy-policy">Vercel's privacy policy</Link>.
 	</p>
 
 	<p class="mb-5 text-lg sm:text-xl">
 		If you sign up for our waitlist using your email address, we use Supabase to securely store and
-		manage your data. For more details, you can review <a
-			href="https://supabase.com/privacy"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="text-indigo-500">Supabase's privacy policy</a
-		>. If you subscribed to Kani's waitlist and would like us to delete your data, please contact us
-		using the email addresses below.
+		manage your data. For more details, you can review
+		<Link url="https://supabase.com/privacy">Supabase's privacy policy</Link>. If you subscribed to
+		Kani's waitlist and would like us to delete your data, please contact us using the email
+		addresses below.
 	</p>
 
 	<p class="mb-5 text-lg sm:text-xl">
@@ -37,12 +34,11 @@
 	</p>
 	<p class="mb-5 text-lg sm:text-xl">
 		Maximilian Hajek:
-		<a href="mailto:mhajek.mmtb-m2024@fh-salzburg.ac.at" class="text-indigo-500"
-			>mhajek.mmtb-m2024@fh-salzburg.ac.at</a
+		<Link url="mailto:mhajek.mmtb-m2024@fh-salzburg.ac.at">mhajek.mmtb-m2024@fh-salzburg.ac.at</Link
 		><br />
 		Pascal Gottschling:
-		<a href="mailto:pgottschling.mmtb-m2024@fh-salzburg.ac.at" class="text-indigo-500"
-			>pgottschling.mmtb-m2024@fh-salzburg.ac.at</a
+		<Link url="mailto:pgottschling.mmtb-m2024@fh-salzburg.ac.at"
+			>pgottschling.mmtb-m2024@fh-salzburg.ac.at</Link
 		>
 	</p>
 	<p class="mb-5 text-lg sm:text-xl">
